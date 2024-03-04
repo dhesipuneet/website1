ea43ba3661b759c3702a0dae390d6af3dd862016
dhesipuneet <143022223+dhesipuneet@users.noreply.github.com>
2024-03-04 02:45:10 +0000

    Sun, Mar 3, 2024, 6:45 PM -08:00

--- js/expanding.js
+++ js/expanding.js
@@ -1,7 +1,15 @@
 panels = document.querySelectorAll('.panel')
 
-panels.forZEach(panel => {
+panels.forEach(panel => {
     panel.addEventListener('click',() => {
+        removeActiceClasses()
         panel.classList.add('active')
     })
 })
+
+
+function removeActiveClasses(){
+    panels.foreach(panel => {
+        panel.classList.remove('active')
+    })
+}
