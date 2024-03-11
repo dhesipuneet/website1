3fded9c678072de2ce1d3bb5ca64080d09330746
dhesipuneet <143022223+dhesipuneet@users.noreply.github.com>
2024-03-11 17:06:11 +0000

    Mon, Mar 11, 2024, 10:06 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,18 +1,28 @@
 toggle = document.getElementById('toggle')
 open = document.getElementById('open')
-model = document.getElementById('model')
+modal = document.getElementById('modal')
 close = document.getElementById('close')
 
 
-
+//toggle nav
 toggle.addEventListner('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+//show modal
 open.addEventListerner('click', () => {
-    modal.classList.add('show-model')
+    modal.classList.add('show-modal')
 })
 
+
+// Hide modal
 close.addEventListener('click', () => {
-    modal.classList.remove('show-model)
+    modal.classList.remove('show-modal')
 })
+
+//hide modal on outside click
+.window.addEventListner('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
+}
