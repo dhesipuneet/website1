ab9c0861169760004c4dc5f05b475295de7abf2f
dhesipuneet <143022223+dhesipuneet@users.noreply.github.com>
2024-03-11 16:56:39 +0000

    Mon, Mar 11, 2024, 9:56 AM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,5 +1,12 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+m0del = document.getElementById('model')
+
 
 toggle.addEventListner('click', () => {
     document.body.classList.toggle('show-nav')
+})
+
+open.addEventListerner('click', () => {
+    modal.classList.add('show-model')
 })
